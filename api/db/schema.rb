2420a70067deb8404eace07f542d0840ee39731b
naoki-00-ito <na.o.ki.00.i.to@gmail.com>
2023-01-30 23:05:22 +0900

    Add: Userテーブル作成

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_30_133350) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_30_140411) do
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
