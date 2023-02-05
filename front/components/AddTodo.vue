2719d9a96b36a09535c267a8a514b5164e3ea9de
naoki-00-ito <na.o.ki.00.i.to@gmail.com>
2023-02-05 15:14:32 +0900

    Add: TODO登録component作成

---
+++ front/components/AddTodo.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-form>
+    <v-container>
+      <v-form>
+        <v-cols col="12" md="4">
+          <v-text-field v-model="title" counter="10" label="todo" required>
+          </v-text-field>
+        </v-cols>
+
+        <v-cols col="12" md="4">
+          <v-btn @click="handleSubmit">作成</v-btn>
+        </v-cols>
+      </v-form>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "",
+    };
+  },
+
+  methods: {
+    handleSubmit() {
+      this.title = "";
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
